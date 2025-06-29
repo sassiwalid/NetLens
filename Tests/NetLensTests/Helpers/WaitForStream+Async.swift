5d3d39df4e1f0b745721a72d1bc62c366290308b
WALID SASSI <sassi.walid@outlook.com>
2025-06-29 19:12:06 +0200

    [NetLens][Async stream helper test] - move wait for stream as standalone method

--- Tests/NetLensTests/Helpers/WaitForStream+Async.swift
+++ Tests/NetLensTests/Helpers/WaitForStream+Async.swift
@@ -11,27 +11,26 @@ enum AsyncTestError: Error {
     case timedOut
 }
 
-extension AsyncStream {
-    func waitForStreamValue<T:Sendable> (from stream: AsyncStream<T>, timeout: TimeInterval = 1.0) async throws -> T {
-        try await withThrowingTaskGroup(of: T?.self) { group in
-
-            group.addTask {
-                var iterator = stream.makeAsyncIterator()
-                
-                return await iterator.next()
-            }
+func waitForStreamValue<T:Sendable> (from stream: AsyncStream<T>, timeout: TimeInterval = 1.0) async throws -> T {
+    try await withThrowingTaskGroup(of: T?.self) { group in
+        
+        group.addTask {
+            var iterator = stream.makeAsyncIterator()
             
-            guard let result = try await group.next() else {
-                throw AsyncTestError.timedOut
-            }
-            
-            group.cancelAll()
-            
-            guard let result else { throw AsyncTestError.timedOut }
-            
-            return result
-
+            return await iterator.next()
         }
+        
+        guard let result = try await group.next() else {
+            throw AsyncTestError.timedOut
+        }
+        
+        group.cancelAll()
+        
+        guard let result else { throw AsyncTestError.timedOut }
+        
+        return result
+        
     }
 }
+
 
--- Tests/NetLensTests/UseCasesTests/ObserveNetworkCallsUseCaseSwiftTests.swift
+++ Tests/NetLensTests/UseCasesTests/ObserveNetworkCallsUseCaseSwiftTests.swift
@@ -11,15 +11,41 @@ import Testing
 
 @Suite("Observe Network Calls use case")
 struct ObserveNetworkCallsUseCaseSwiftTests {
+
     private let mockNetworkrepository: MockNetworkCallRepository
-    
-    private let getNetworkCallsUseCase: GetNetworkCallsUseCaseImpl
-    
+
+    private let observeNetworkCallsUseCase: ObserveNetworkCallsUseCaseImpl
+
     init() {
-        
+
         mockNetworkrepository = MockNetworkCallRepository()
+
+        observeNetworkCallsUseCase = ObserveNetworkCallsUseCaseImpl(networkCallRepository: mockNetworkrepository)
+    }
+
+    @Test("Execute return stream from repository")
+    func execureReturnstream() async throws {
+
+        let call = NetworkCallFactory.makeSuccessCall()
+
+        let stream = observeNetworkCallsUseCase.execute()
+
+        let streamTask = Task {
+            try await waitForStreamValue(
+                from: stream,
+                timeout: 2.0
+            )
+            
+        }
+
+        await mockNetworkrepository.addNetworkCall(call)
+
+        let receivedCalls = try await streamTask.value
+
+        #expect(receivedCalls.count == 1)
+
+        #expect(receivedCalls.first?.id == call.id)
         
-        getNetworkCallsUseCase = GetNetworkCallsUseCaseImpl(networkCallRepository: mockNetworkrepository)
     }
     
     
